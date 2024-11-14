337b422395a0dee217a3d7bc97eba0c526892761
ksenia-zh13579 <kseniazuzleva2395@gmail.com>
2024-11-14 18:20:34 +0300

    add operations

--- task5.go
+++ task5.go
@@ -3,5 +3,28 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello there")
+	var n1, n2 float64
+	fmt.Println("Input two numbers: ")
+	fmt.Scan(&n1, &n2)
+
+	var op string
+	fmt.Println("Input operation: ")
+	fmt.Scan(&op)
+
+	switch op {
+	case "+":
+		fmt.Println(n1, "+", n2, "=", n1+n2)
+	case "-":
+		fmt.Println(n1, "-", n2, "=", n1-n2)
+	case "*":
+		fmt.Println(n1, "*", n2, "=", n1*n2)
+	case "/":
+		{
+			if n2 == 0 {
+				fmt.Println("Sorry, the division by zero is prohibited!!")
+			} else {
+				fmt.Println(n1, "/", n2, "=", n1/n2)
+			}
+		}
+	}
 }
